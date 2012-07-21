82e9d4c3fde34bea710c167867a27b405027fadf
Stephen Visser <stephen@orderoo.com>
2012-07-21 13:00:04 -0600

    Added map, brah!

---
+++ TIMapController.h
@@ -0,0 +1,13 @@
+//
+//  TIMapController.h
+//  TourIt
+//
+//  Created by Stephen Visser on 12-07-21.
+//  Copyright (c) 2012 DreamTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIMapController : UIViewController
+
+@end
---
+++ TIMapController.m
@@ -0,0 +1,40 @@
+//
+//  TIMapController.m
+//  TourIt
+//
+//  Created by Stephen Visser on 12-07-21.
+//  Copyright (c) 2012 DreamTeam. All rights reserved.
+//
+
+#import "TIMapController.h"
+
+@interface TIMapController ()
+@property (weak, nonatomic) IBOutlet MKMapView *map;
+
+@end
+
+@implementation TIMapController
+@synthesize map;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
