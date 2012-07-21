e1546b2e7224fcc5ed2ad81bf828e3b8535c0f27
Stephen Visser <stephen@orderoo.com>
2012-07-21 12:58:06 -0600

    CODE CRUSHING FOR LOLZ!

--- TourIt/TIViewController.m
+++ TourIt/TIViewController.m
@@ -8,11 +8,20 @@
 
 #import "TIViewController.h"
 
+
+// FIXME: Dummy data 
+static NSArray *dummyData;
+
 @interface TIViewController ()
 
 @end
 
 @implementation TIViewController
+
++ (void)initialize
+{
+    dummyData = @[@"Dragons", @"Celebrities", @"Brett's Children"];
+}
 
 - (void)viewDidLoad
 {
@@ -24,6 +33,51 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section)
+    {
+        return 44;
+    }
+    else
+    {
+        return 140;
+    }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section)
+    {
+        return [dummyData count];
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section)
+    {
+        UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"map" forIndexPath:indexPath];
+        cell.textLabel.text = dummyData[indexPath.row];
+        return cell;
+    }
+    else
+    {
+        UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"tag" forIndexPath:indexPath];
+        cell.textLabel.text = @"Map, yo!";
+        return cell;
+    }
 }
 
 @end
